c635229639d0f8d364a0e6c4a7cf5c156be311e6
ajeet2012 <ajeetmca2012@gmail.com>
2026-04-02 22:19:55 +0530

    Project setup and add AppIcons and Colors in assets

---
+++ Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Ajeet Sharma on 02/04/26.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
